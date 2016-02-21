e73269554b7f794fa6ee8f5f5a8dfc9789e0e47a
Danny Guinther <dannyguinther@gmail.com>
2016-02-21 11:51:50 -0500

    Disable most keyboard commands and controls

--- src/js/app/config.js
+++ src/js/app/config.js
@@ -1,22 +1,12 @@
 let config = {
   reveal: {
+    controls: false,
     height: 720,
+    help: false,
     history: true,
-    keyboard: {
-      //27: function() { console.log("ESC pressed"); return true },
-      //37: function() { console.log("LEFT pressed"); return true },
-      //38: function() { console.log("UP pressed"); return true },
-      //39: function() { console.log("RIGHT pressed"); return true },
-      //40: function() { console.log("DOWN pressed"); return true },
-      //66: function() { console.log("b pressed"); return true },
-      // disable overview
-      27: function() { return false; },
-      79: function() { return false; }
-      //116: function() { console.log("F5 pressed"); return true }
-    },
-    onReady: function() {
-      window.focus();
-    },
+    keyboard: false,
+    overview: false,
+    touch: false,
     transition: "convex",
     width: 1280
   },
