ee47b8c26c6fbfd3029e89c87cfbd48d3f9c9258
Danny Guinther <dannyguinther@gmail.com>
2016-03-07 08:18:48 -0500

    Add SandboxSlide class

---
+++ src/js/framework/sandbox-slide.js
@@ -0,0 +1,56 @@
+import {slideshow} from "../framework/slideshow";
+import Slide from "../framework/slide";
+
+class SandboxSlide extends Slide {
+  constructor(config) {
+    super(config)
+    this.completed = false;
+    registerListener(this);
+  }
+
+  activate(event) {
+    if (this.completed) { return; }
+    slideshow.hideControls();
+  }
+
+  renderComplete(event) {
+    if (this.validateContext(event.context)) {
+      this.completed = true;
+      hideInstructions(this.element);
+      slideshow.showControls();
+    }
+  }
+
+  renderStart(event) {}
+
+  validateContext(context) {
+    return true;
+  }
+}
+
+function discoverIFrameDocument(element) {
+  let iframe = element.querySelector("iframe");
+  let iframeContent = iframe.contentWindow ||
+    iframe.contentDocument.document ||
+    iframe.contentDocument;
+  return iframeContent.document;
+}
+
+function hideInstructions(element) {
+  let structs = element.querySelectorAll(".unlock-instructions");
+  for (let i = 0, length = structs.length; i < structs.length; ++i) {
+    structs[i].classList.add("invisible");
+  }
+}
+
+function registerListener(sandboxSlide) {
+  let iFrameDoc = discoverIFrameDocument(sandboxSlide.element);
+  iFrameDoc.addEventListener("spyRegister", function(event) {
+    sandboxSlide.renderStart(event);
+  });
+  iFrameDoc.addEventListener("spyComplete", function(event) {
+    sandboxSlide.renderComplete(event);
+  });
+}
+
+export default SandboxSlide;
